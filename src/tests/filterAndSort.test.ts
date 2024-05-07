125610a25f979398de893af0fb78e361fd8df908
Zhi Yang <zhiyang@kitesense.sg>
2024-05-07 20:41:46 +0800

    finish unit tests

--- src/tests/filterAndSort.test.ts
+++ src/tests/filterAndSort.test.ts
@@ -171,3 +171,79 @@ test("filterAndSort with name filter", () => {
     },
   ]);
 });
+
+test("filterAndSort with desc order", () => {
+  const hotel: Hotel[] = [
+    {
+      id: 1,
+      name: "Hotel 1",
+      rating: 4,
+      stars: 4,
+      address: "Address 1",
+      photo: "photo1",
+      price: 100,
+      description: "Description 1",
+      reviews: [],
+    },
+    {
+      id: 2,
+      name: "Hotel 2",
+      rating: 3,
+      stars: 3,
+      address: "Address 2",
+      photo: "photo2",
+      price: 200,
+      description: "Description 2",
+      reviews: [],
+    },
+    {
+      id: 3,
+      name: "Hotel 3",
+      rating: 5,
+      stars: 5,
+      address: "Address 3",
+      photo: "photo3",
+      price: 150,
+      description: "Description 3",
+      reviews: [],
+    },
+  ];
+  const filters: Filter = { name: "", rating: 0, minPrice: 0, maxPrice: 200 };
+  const sort: Sort = { price: "desc" };
+  const result = filterAndSort(hotel, filters, sort);
+  expect(result).toEqual([
+    {
+      id: 2,
+      name: "Hotel 2",
+      rating: 3,
+      stars: 3,
+      address: "Address 2",
+      photo: "photo2",
+      price: 200,
+      description: "Description 2",
+      reviews: [],
+    },
+    {
+      id: 3,
+      name: "Hotel 3",
+      rating: 5,
+      stars: 5,
+      address: "Address 3",
+      photo: "photo3",
+      price: 150,
+      description: "Description 3",
+      reviews: [],
+    },
+    {
+      id: 1,
+      name: "Hotel 1",
+      rating: 4,
+      stars: 4,
+      address: "Address 1",
+      photo: "photo1",
+      price: 100,
+      description: "Description 1",
+      reviews: [],
+    },
+  ]);
+});
